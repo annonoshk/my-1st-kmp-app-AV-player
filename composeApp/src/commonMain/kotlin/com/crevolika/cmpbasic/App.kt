114c016b3542910b2d60398d320db9c1cc9c57c4
huckk <huck.kim@kangwon.ac.kr>
2024-12-17 01:34:34 +0900

    Desktop , wasm, Android build complete.

--- composeApp/src/commonMain/kotlin/com/crevolika/cmpbasic/App.kt
+++ composeApp/src/commonMain/kotlin/com/crevolika/cmpbasic/App.kt
@@ -5,33 +5,44 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
+import kotlinproject.composeapp.generated.resources.logo
+import kotlinproject.composeapp.generated.resources.privateeye
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        var showImage by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
+
+            Divider(color = Color.Gray, thickness = 2.dp)
+            Image(painter = painterResource(Res.drawable.privateeye), contentDescription = null)
+            Text("This is Crevolika Inc. preparation app")
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Image(painterResource(Res.drawable.logo), null)
+                    Text("Private Eye to follow Deep Fake: $greeting")
                 }
             }
         }
     }
 }
+
--- composeApp/src/desktopMain/kotlin/com/crevolika/cmpbasic/main.kt
+++ composeApp/src/desktopMain/kotlin/com/crevolika/cmpbasic/main.kt
@@ -2,12 +2,17 @@ package com.crevolika.cmpbasic
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.privateeye
+import org.jetbrains.compose.resources.painterResource
+
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "KotlinProject",
-    ) {
+        title = "CrevoLika",
+        icon = painterResource(Res.drawable.privateeye)
+    ){
         App()
     }
 }
